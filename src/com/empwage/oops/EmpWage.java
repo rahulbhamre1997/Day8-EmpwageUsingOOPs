51ffe73b16051c4c8ff999e52a5b44020dcec5ff
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-03-10 17:25:45 +0530

    Added:Employee salary per day

--- src/com/empwage/oops/EmpWage.java
+++ src/com/empwage/oops/EmpWage.java
@@ -6,15 +6,29 @@ public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee wage problem using OOPs");
 		
-		Random ran = new Random();
-		int present = 1;
-		int empCheck = ran.nextInt(2);
-		if (empCheck == present) {
-			System.out.println("Employee is Present");
+		int empCheck = Employee.empCheck();
+		Employee.empSalary(empCheck);
+	}
+
+}
+	class Employee {
+		static final int is_full_time = 1;
+		static final int Emp_Rate_Per_Hr= 20;
+
+	public static int empCheck() {
+		Random random = new Random();
+		int empCheck = random.nextInt(2);
+		return empCheck;
+	}
+
+	public static void empSalary(int empCheck) {
+		int empWrkHrs = 0;
+		if (empCheck == is_full_time) {
+			empWrkHrs = 8;
+		} else {
+			empWrkHrs = 0;
 		}
-		else {
-			System.out.println("Employee is Absent");
-		}
-	
+		int empWage = empWrkHrs * Emp_Rate_Per_Hr;
+		System.out.println("empWage = " + empWage);
 	}
 }
