cb1bb551fd53c8b45eba29d4bbdc9ebb17df406e
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-03-10 17:48:10 +0530

    Added:Using switch case employee one day salary

--- src/com/empwage/oops/EmpWage.java
+++ src/com/empwage/oops/EmpWage.java
@@ -5,12 +5,11 @@ import java.util.Random;
 public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee wage problem using OOPs");
-		
 		int empCheck = Employee.empCheck();
 		Employee.empSalary(empCheck);
-	}
+	    }
 
-}
+	}
 	class Employee {
 		static final int is_full_time = 1;
 		static final int is_part_time = 2;
@@ -21,15 +20,12 @@ public class EmpWage {
 		int empCheck = random.nextInt(3);
 		return empCheck;
 	}
-
 	public static void empSalary(int empCheck) {
-		int empWrkHrs = 0;
-		if (empCheck == is_full_time) {
-			empWrkHrs = 8;
-		} else if (empCheck == is_part_time) {
-			empWrkHrs = 4;
-		} else {
-			empWrkHrs = 0;
+		int empWrkHrs;
+		switch (empCheck) {
+		case is_full_time -> empWrkHrs = 8;
+		case is_part_time -> empWrkHrs = 4;
+		default -> empWrkHrs = 0;
 		}
 		int empWage = empWrkHrs * Emp_Rate_Per_Hr;
 		System.out.println("empWage = " + empWage);
