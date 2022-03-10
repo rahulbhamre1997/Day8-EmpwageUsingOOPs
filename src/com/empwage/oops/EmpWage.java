408679872bc5fbfb31e2f4b0783086b746899ba3
rahulbhamre1997 <rahul1997bhamre@gmail.com>
2022-03-10 17:14:36 +0530

    Added:To check Employee is present or absent

--- src/com/empwage/oops/EmpWage.java
+++ src/com/empwage/oops/EmpWage.java
@@ -1,7 +1,20 @@
 package com.empwage.oops;
 
+import java.util.Random;
+
 public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee wage problem using OOPs");
+		
+		Random ran = new Random();
+		int present = 1;
+		int empCheck = ran.nextInt(2);
+		if (empCheck == present) {
+			System.out.println("Employee is Present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
+	
 	}
 }
